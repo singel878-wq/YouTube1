cd58736e242aab98bce3a9c7aaa7ab49c39666ec
singel878-wq <singel878@gmail.com>
2026-02-05 22:25:03 +0330

    Update index.js

--- index.js
+++ index.js
@@ -723,3 +723,8 @@ const PORT = process.env.PORT || 3000;
 appServer.listen(PORT, () => {
     console.log("Server running on port " + PORT);
 });
+const PORT = process.env.PORT || 3000;
+
+appServer.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
+});
