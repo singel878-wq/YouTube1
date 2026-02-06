fd2f204466ad68c9f5a9d3b78a1eebaea5b60695
singel878-wq <singel878@gmail.com>
2026-02-06 19:31:27 +0330

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+
+module.exports = {
+
+    BOT_TOKEN: process.env.BOT_TOKEN,
+
+    CHAT_ID: process.env.CHAT_ID,
+
+    GALLERY_PATH: "./gallery",
+
+    MAX_BATCH: 10
+
+};
